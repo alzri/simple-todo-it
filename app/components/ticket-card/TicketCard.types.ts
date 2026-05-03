0e3525e712617e9a04e97c6e88c18ac698120cb8
azrinka <alzrinka1@gmail.com>
2026-05-03 22:47:01 +0200

    update

--- app/components/ticket-card/TicketCard.types.ts
+++ app/components/ticket-card/TicketCard.types.ts
@@ -6,4 +6,7 @@ export interface ITicketCardProps extends React.HTMLAttributes<HTMLDivElement> {
   desc: string;
   children?: React.ReactNode;
   status: TicketStatuses;
+  onStatusChange: (status: TicketStatuses) => void;
+  id: string;
+  onEdit?: (id: string, title: string, desc: string) => void;
 }
--- app/components/ticket-column/TicketColumn.tsx
+++ app/components/ticket-column/TicketColumn.tsx
@@ -4,26 +4,32 @@ import { TicketStatuses } from '../ticket-status/TicketStatus.types';
 import styles from './TicketColumn.module.scss';
 import clsx from 'clsx';
 
-interface ITicketColumnPRops {
+interface ITicketColumnProps {
   tickets: ITicketCardProps[];
   status: TicketStatuses;
+  changeStatus: (id: string, status: TicketStatuses) => void;
+  editTicket: (id: string, title: string, desc: string) => void;
 }
 
-export const TicketColumn = ({ tickets, status }: ITicketColumnPRops) => {
+export const TicketColumn = ({ tickets, status, changeStatus, editTicket }: ITicketColumnProps) => {
   return (
     <div className={styles['column']}>
       <h2 className={clsx(styles['column-title'], styles[`${status}`])}>
         {status.replace(/-/g, ' ')}
       </h2>
+
       <div className={styles['ticket-list']}>
         {tickets
           .filter((ticket) => ticket.status === status)
           .map((ticket) => (
             <TicketCard
               key={ticket.id}
+              id={ticket.id}
               title={ticket.title}
               desc={ticket.desc}
               status={ticket.status}
+              onStatusChange={(newStatus) => changeStatus(ticket.id, newStatus)}
+              onEdit={editTicket}
             />
           ))}
       </div>
